e021df46ef5b481996feb5f4eccaa5ac6f840d74
hector pollo <pollo2419@hotmail.com>
2019-08-29 15:15:09 -0500

    Listo

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -34,9 +34,7 @@
             this.b4 = new System.Windows.Forms.Button();
             this.b5 = new System.Windows.Forms.Button();
             this.b6 = new System.Windows.Forms.Button();
-            this.b1 = new System.Windows.Forms.Button();
             this.b2 = new System.Windows.Forms.Button();
-            this.b3 = new System.Windows.Forms.Button();
             this.bp = new System.Windows.Forms.Button();
             this.b0 = new System.Windows.Forms.Button();
             this.restar = new System.Windows.Forms.Button();
@@ -46,10 +44,10 @@
             this.bce = new System.Windows.Forms.Button();
             this.igual = new System.Windows.Forms.Button();
             this.multiplicar = new System.Windows.Forms.Button();
-            this.sumar = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.bt1 = new System.Windows.Forms.Button();
-            this.bt3 = new System.Windows.Forms.Button();
+            this.porcentaje = new System.Windows.Forms.Button();
+            this.ventana = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // b7
@@ -72,6 +70,7 @@
             this.b8.TabIndex = 1;
             this.b8.Text = "8";
             this.b8.UseVisualStyleBackColor = false;
+            this.b8.Click += new System.EventHandler(this.B8_Click);
             // 
             // b9
             // 
@@ -82,6 +81,7 @@
             this.b9.TabIndex = 2;
             this.b9.Text = "9";
             this.b9.UseVisualStyleBackColor = false;
+            this.b9.Click += new System.EventHandler(this.B9_Click);
             // 
             // b4
             // 
@@ -92,6 +92,7 @@
             this.b4.TabIndex = 3;
             this.b4.Text = "4";
             this.b4.UseVisualStyleBackColor = false;
+            this.b4.Click += new System.EventHandler(this.B4_Click);
             // 
             // b5
             // 
@@ -102,6 +103,7 @@
             this.b5.TabIndex = 4;
             this.b5.Text = "5";
             this.b5.UseVisualStyleBackColor = false;
+            this.b5.Click += new System.EventHandler(this.B5_Click);
             // 
             // b6
             // 
@@ -112,13 +114,7 @@
             this.b6.TabIndex = 5;
             this.b6.Text = "6";
             this.b6.UseVisualStyleBackColor = false;
-            // 
-            // b1
-            // 
-            this.b1.Location = new System.Drawing.Point(0, 0);
-            this.b1.Name = "b1";
-            this.b1.Size = new System.Drawing.Size(75, 23);
-            this.b1.TabIndex = 21;
+            this.b6.Click += new System.EventHandler(this.B6_Click);
             // 
             // b2
             // 
@@ -129,14 +125,7 @@
             this.b2.TabIndex = 7;
             this.b2.Text = "2";
             this.b2.UseVisualStyleBackColor = false;
-            // 
-            // b3
-            // 
-            this.b3.Location = new System.Drawing.Point(0, 0);
-            this.b3.Name = "b3";
-            this.b3.Size = new System.Drawing.Size(75, 23);
-            this.b3.TabIndex = 20;
-            this.b3.Click += new System.EventHandler(this.B3_Click);
+            this.b2.Click += new System.EventHandler(this.B2_Click);
             // 
             // bp
             // 
@@ -147,6 +136,7 @@
             this.bp.TabIndex = 9;
             this.bp.Text = ".";
             this.bp.UseVisualStyleBackColor = false;
+            this.bp.Click += new System.EventHandler(this.Bp_Click);
             // 
             // b0
             // 
@@ -167,6 +157,7 @@
             this.restar.TabIndex = 11;
             this.restar.Text = "-";
             this.restar.UseVisualStyleBackColor = false;
+            this.restar.Click += new System.EventHandler(this.Restar_Click);
             // 
             // dividir
             // 
@@ -177,6 +168,7 @@
             this.dividir.TabIndex = 12;
             this.dividir.Text = "/";
             this.dividir.UseVisualStyleBackColor = false;
+            this.dividir.Click += new System.EventHandler(this.Dividir_Click);
             // 
             // mas
             // 
@@ -187,6 +179,7 @@
             this.mas.TabIndex = 13;
             this.mas.Text = "+";
             this.mas.UseVisualStyleBackColor = false;
+            this.mas.Click += new System.EventHandler(this.Mas_Click);
             // 
             // bc
             // 
@@ -197,6 +190,7 @@
             this.bc.TabIndex = 14;
             this.bc.Text = "C";
             this.bc.UseVisualStyleBackColor = false;
+            this.bc.Click += new System.EventHandler(this.Bc_Click);
             // 
             // bce
             // 
@@ -207,6 +201,7 @@
             this.bce.TabIndex = 15;
             this.bce.Text = "Ce";
             this.bce.UseVisualStyleBackColor = false;
+            this.bce.Click += new System.EventHandler(this.Bce_Click);
             // 
             // igual
             // 
@@ -217,6 +212,7 @@
             this.igual.TabIndex = 16;
             this.igual.Text = "=";
             this.igual.UseVisualStyleBackColor = false;
+            this.igual.Click += new System.EventHandler(this.Igual_Click);
             // 
             // multiplicar
             // 
@@ -227,52 +223,56 @@
             this.multiplicar.TabIndex = 17;
             this.multiplicar.Text = "*";
             this.multiplicar.UseVisualStyleBackColor = false;
+            this.multiplicar.Click += new System.EventHandler(this.Multiplicar_Click);
             // 
-            // sumar
+            // porcentaje
             // 
-            this.sumar.BackColor = System.Drawing.Color.Cornsilk;
-            this.sumar.Location = new System.Drawing.Point(320, 140);
-            this.sumar.Name = "sumar";
-            this.sumar.Size = new System.Drawing.Size(51, 32);
-            this.sumar.TabIndex = 18;
-            this.sumar.Text = "+";
-            this.sumar.UseVisualStyleBackColor = false;
+            this.porcentaje.BackColor = System.Drawing.Color.Cornsilk;
+            this.porcentaje.Location = new System.Drawing.Point(320, 140);
+            this.porcentaje.Name = "porcentaje";
+            this.porcentaje.Size = new System.Drawing.Size(51, 32);
+            this.porcentaje.TabIndex = 18;
+            this.porcentaje.Text = "%";
+            this.porcentaje.UseVisualStyleBackColor = false;
+            this.porcentaje.Click += new System.EventHandler(this.Porcentaje_Click);
             // 
-            // textBox1
+            // ventana
             // 
-            this.textBox1.Location = new System.Drawing.Point(27, 27);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(322, 20);
-            this.textBox1.TabIndex = 19;
+            this.ventana.Location = new System.Drawing.Point(27, 27);
+            this.ventana.Name = "ventana";
+            this.ventana.Size = new System.Drawing.Size(322, 20);
+            this.ventana.TabIndex = 19;
             // 
-            // bt1
+            // b1
             // 
-            this.bt1.BackColor = System.Drawing.Color.Cornsilk;
-            this.bt1.Location = new System.Drawing.Point(12, 140);
-            this.bt1.Name = "bt1";
-            this.bt1.Size = new System.Drawing.Size(51, 32);
-            this.bt1.TabIndex = 22;
-            this.bt1.Text = "1";
-            this.bt1.UseVisualStyleBackColor = false;
+            this.b1.BackColor = System.Drawing.Color.Cornsilk;
+            this.b1.Location = new System.Drawing.Point(12, 140);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(51, 32);
+            this.b1.TabIndex = 22;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.B1_Click);
             // 
-            // bt3
+            // b3
             // 
-            this.bt3.BackColor = System.Drawing.Color.Cornsilk;
-            this.bt3.Location = new System.Drawing.Point(126, 140);
-            this.bt3.Name = "bt3";
-            this.bt3.Size = new System.Drawing.Size(51, 32);
-            this.bt3.TabIndex = 23;
-            this.bt3.Text = "3";
-            this.bt3.UseVisualStyleBackColor = false;
+            this.b3.BackColor = System.Drawing.Color.Cornsilk;
+            this.b3.Location = new System.Drawing.Point(126, 140);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(51, 32);
+            this.b3.TabIndex = 23;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.B3_Click_1);
             // 
             // CALCULADORA
             // 
             this.BackColor = System.Drawing.Color.Crimson;
             this.ClientSize = new System.Drawing.Size(384, 243);
-            this.Controls.Add(this.bt3);
-            this.Controls.Add(this.bt1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.sumar);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.ventana);
+            this.Controls.Add(this.porcentaje);
             this.Controls.Add(this.multiplicar);
             this.Controls.Add(this.igual);
             this.Controls.Add(this.bce);
@@ -282,9 +282,7 @@
             this.Controls.Add(this.restar);
             this.Controls.Add(this.b0);
             this.Controls.Add(this.bp);
-            this.Controls.Add(this.b3);
             this.Controls.Add(this.b2);
-            this.Controls.Add(this.b1);
             this.Controls.Add(this.b6);
             this.Controls.Add(this.b5);
             this.Controls.Add(this.b4);
@@ -325,9 +323,7 @@
         private System.Windows.Forms.Button b4;
         private System.Windows.Forms.Button b5;
         private System.Windows.Forms.Button b6;
-        private System.Windows.Forms.Button b1;
         private System.Windows.Forms.Button b2;
-        private System.Windows.Forms.Button b3;
         private System.Windows.Forms.Button bp;
         private System.Windows.Forms.Button b0;
         private System.Windows.Forms.Button restar;
@@ -337,10 +333,10 @@
         private System.Windows.Forms.Button bce;
         private System.Windows.Forms.Button igual;
         private System.Windows.Forms.Button multiplicar;
-        private System.Windows.Forms.Button sumar;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button bt1;
-        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button porcentaje;
+        private System.Windows.Forms.TextBox ventana;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b3;
     }
 }
 
--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -12,6 +12,10 @@ namespace Calculadora
 {
     public partial class CALCULADORA : Form
     {
+        double numer1;
+        double numer2;
+        double resulta;
+        string op;
         public CALCULADORA()
         {
             InitializeComponent();
@@ -24,12 +28,139 @@ namespace Calculadora
 
         private void B7_Click(object sender, EventArgs e)
         {
-
+            ventana.Text = ventana.Text + "7";
         }
 
         private void B3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Bp_Click(object sender, EventArgs e)
+        {
+            ventana.Text = ventana.Text + ".";
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            ventana.Text = ventana.Text + "1";
+        }
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            ventana.Text = ventana.Text + "2";
+        }
+
+        private void B3_Click_1(object sender, EventArgs e)
+        {
+            ventana.Text = ventana.Text + "3";
+        }
+
+        private void B4_Click(object sender, EventArgs e)
+        {
+            ventana.Text = ventana.Text + "4";
+        }
+
+        private void B5_Click(object sender, EventArgs e)
+        {
+            ventana.Text = ventana.Text + "5";
+        }
+
+        private void B6_Click(object sender, EventArgs e)
+        {
+            ventana.Text = ventana.Text + "6";
+        }
+
+        private void B8_Click(object sender, EventArgs e)
+        {
+            ventana.Text = ventana.Text + "8";
+        }
+
+        private void B9_Click(object sender, EventArgs e)
+        {
+            ventana.Text = ventana.Text + "9";
+        }
+
+        private void Dividir_Click(object sender, EventArgs e)
+        {
+            op = "/";
+            numer1 = double.Parse(ventana.Text);
+            ventana.Clear();
+        }
+
+        private void Mas_Click(object sender, EventArgs e)
+        {
+            op = "+";
+            numer1 = double.Parse(ventana.Text);
+            ventana.Clear();
+        }
+
+        private void Multiplicar_Click(object sender, EventArgs e)
+        {
+            op = "*";
+            numer1 = double.Parse(ventana.Text);
+            ventana.Clear();
+        }
+
+        private void Porcentaje_Click(object sender, EventArgs e)
+        {
+            op = "%";
+            numer1 = double.Parse(ventana.Text);
+            ventana.Clear();
+        }
+
+        private void Restar_Click(object sender, EventArgs e)
+        {
+            op = "-";
+            numer1 = double.Parse(ventana.Text);
+            ventana.Clear();
+        }
+
+        private void Igual_Click(object sender, EventArgs e)
+        {
+            numer2 = double.Parse(ventana.Text);
+            if (op=="+")
+            {
+                resulta = numer1 + numer2;
+                ventana.Text = resulta.ToString();
+            }
+            else if (op=="-")
+            {
+                resulta = numer1 - numer2;
+                ventana.Text = resulta.ToString();
+            }
+            else if (op=="*")
+            {
+                resulta = numer1 * numer2;
+                ventana.Text = resulta.ToString();
+            }
+            else if (op=="/")
+            {
+                resulta = numer1 / numer2;
+                ventana.Text = resulta.ToString();
+            }
+            else if (op=="%")
+            {
+                resulta = (numer1/100) * numer2;
+                ventana.Text = resulta.ToString();
+            }
+        }
+
+        private void Bc_Click(object sender, EventArgs e)
+        {
+            ventana.Clear();
+        }
+
+        private void Bce_Click(object sender, EventArgs e)
+        {
+            if (ventana.Text.Length==1)
+            {
+                ventana.Text = "";
+            }
+            else
+            {
+                ventana.Text = ventana.Text.Substring(0, ventana.Text.Length);
+            }
         }
     }
 }
